45197b0cd8d8e6d039cfd2d17b22fd23fea04bfc
droidhackzor <gentooguy@gmail.com>
2023-02-05 13:21:34 -0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'SleeplessGames Studios'
+copyright = '2023, Sleepless Games'
+author = 'Sleepless Games'
 
 release = '0.1'
 version = '0.1.0'
